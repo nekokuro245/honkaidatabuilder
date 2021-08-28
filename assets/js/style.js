eadb307c54769f10c5fedf30edd41ff8f748290e
Your Name <nekokuro245@gmail.com>
2021-08-28 23:36:34 +0700

    Update 11:36 28/08/2021

---
+++ assets/js/style.js
@@ -0,0 +1,14 @@
+function openHonkai(evt, honkaiName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(honkaiName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
