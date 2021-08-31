545f8a11255e12bc73ab2f05ae98c3ded298aa2f
Your Name <nekokuro245@gmail.com>
2021-08-31 18:42:16 +0700

    Update 6:41 PM 31/08/2021

--- assets/js/style.js
+++ assets/js/style.js
@@ -12,3 +12,44 @@ function openHonkai(evt, honkaiName) {
   evt.currentTarget.className += " active";
 }
 
+document.getElementById("intro").addEventListener("mouseover", mouseOver);
+document.getElementById("intro").addEventListener("mouseout", mouseOut);
+
+function mouseOver() {
+document.getElementById("heading-intro").style.display = "flex";}
+
+function mouseOut() {
+document.getElementById("heading-intro").style.display = "none";}
+
+
+// Mở chatbox
+const chatBoxbtn = document.querySelector('.js-chatbox')
+const modal = document.querySelector('.js-modal')
+const closeChatbox = document.querySelector('.js-close-chatbox')
+
+// Hàm hiển thị chatbox (thêm class open vào modal)
+function showChatbox(){
+  modal.classList.add('open')
+}
+
+// Hàm ẩn chatbox (gỡ bỏ class open của modal)
+function hideChatbox(){
+  modal.classList.remove('open')
+}
+
+//Nghe hành vi click, vào button
+chatBoxbtn.addEventListener('click', showChatbox)
+closeChatbox.addEventListener('click', hideChatbox)
+
+// $(window).scroll(function () {
+//   if($(window).scrollTop() > 0) {
+//     element.addClass("animateMe");
+//   }
+//   else {
+//     element.removeClass("animateMe");
+//   }
+// });
+
+// function myFunction() {
+//   document.getElementById("myDIV").style.overflowY = "scroll";
+// }
