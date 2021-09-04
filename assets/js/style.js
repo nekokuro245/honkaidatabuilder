433e2c049cd6f1261ba1d45fb63c434f61517f5c
Your Name <nekokuro245@gmail.com>
2021-09-05 01:20:35 +0700

    Update 1:20 AM 05/09/2021

--- assets/js/style.js
+++ assets/js/style.js
@@ -24,22 +24,55 @@ document.getElementById("heading-intro").style.display = "none";}
 
 // Mở chatbox
 const chatBoxbtn = document.querySelector('.js-chatbox')
-const modal = document.querySelector('.js-modal')
 const closeChatbox = document.querySelector('.js-close-chatbox')
+const modal = document.querySelector('.js-modal')
+const modalContainer = document.querySelector('.modal-container')
+
+// chatBoxbtn.onclick = function(){
+//     modalContainer.style.transform = "translateX(0)";
+//     modal.style.display="flex";
+// }
+
+// closeChatbox.onclick = function(){
+//     modalContainer.style.transform = "translateX(100%)";
+//     modal.style.display="none";
+// }
 
 // Hàm hiển thị chatbox (thêm class open vào modal)
 function showChatbox(){
   modal.classList.add('open')
+  modalContainer.style.transform="translateX(0)";
 }
 
 // Hàm ẩn chatbox (gỡ bỏ class open của modal)
 function hideChatbox(){
   modal.classList.remove('open')
+  modalContainer.style.transform="translateX(100%)";
 }
 
-//Nghe hành vi click, vào button
+// Nghe hành vi click, vào button
 chatBoxbtn.addEventListener('click', showChatbox)
 closeChatbox.addEventListener('click', hideChatbox)
+
+
+// Thực hiện animation khi mử
+// chatBoxbtn.onclick = function(){
+//   var isChangedOpen = modalContainer.ClientAnimation === modalContainerAnimation;
+//   if (isChangedOpen){
+    
+//     modalContainer.style.animation = 'modalFadeout ease 1s';
+//   }else{
+//     closeChatbox.onclick = function(){
+//       modalContainer.style.animation = 'null';
+//     }
+//   }
+// }
+// Thực hiện animation khi đóng
+// closeChatbox.onclick = function(){
+//   if (modalContainerAnimation){
+//     modalContainer.style.animation = 'modalFadeout ease 1s';
+//   }
+// }
 
 // $(window).scroll(function () {
 //   if($(window).scrollTop() > 0) {
