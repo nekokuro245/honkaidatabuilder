d140c3d448bb2f8fd899a70dc81664479baddac8
Your Name <nekokuro245@gmail.com>
2021-09-10 21:04:34 +0700

    Update 9:04PM 10/09/2021

--- assets/js/style.js
+++ assets/js/style.js
@@ -12,6 +12,7 @@ function openHonkai(evt, honkaiName) {
   evt.currentTarget.className += " active";
 }
 
+// Khi hover vô slider thì hiện thanh tìm kiếm
 document.getElementById("intro").addEventListener("mouseover", mouseOver);
 document.getElementById("intro").addEventListener("mouseout", mouseOut);
 
@@ -22,21 +23,80 @@ function mouseOut() {
 document.getElementById("heading-intro").style.display = "none";}
 
 
+// Khi hover thì đổi box và rotate icon 1
+const changeRotate1 = document.getElementById("social-bar-1");
+const iconChangerotate1 = document.getElementById("icon-rotate-js1");
+
+changeRotate1.addEventListener("mouseover", rotateIn1);
+changeRotate1.addEventListener("mouseout", rotateOut1);
+
+function rotateIn1(){
+  changeRotate1.style.width = "400px";
+  iconChangerotate1.style.transform = 'rotate(0deg)';
+}
+
+function rotateOut1(){
+  changeRotate1.style.width = "100px";
+  iconChangerotate1.style.transform = 'rotate(360deg)';
+}
+
+// Khi hover thì đổi box và rotate icon 2
+const changeRotate2 = document.getElementById("social-bar-2");
+const iconChangerotate2 = document.getElementById("icon-rotate-js2");
+
+changeRotate2.addEventListener("mouseover", rotateIn2);
+changeRotate2.addEventListener("mouseout", rotateOut2);
+
+function rotateIn2(){
+  changeRotate2.style.width = "400px";
+  iconChangerotate2.style.transform = 'rotate(0deg)';
+}
+
+function rotateOut2(){
+  changeRotate2.style.width = "100px";
+  iconChangerotate2.style.transform = 'rotate(360deg)';
+}
+
+// Khi hover thì đổi box và rotate icon 3
+const changeRotate3 = document.getElementById("social-bar-3");
+const iconChangerotate3 = document.getElementById("icon-rotate-js3");
+
+changeRotate3.addEventListener("mouseover", rotateIn3);
+changeRotate3.addEventListener("mouseout", rotateOut3);
+
+function rotateIn3(){
+  changeRotate3.style.width = "400px";
+  iconChangerotate3.style.transform = 'rotate(0deg)';
+}
+
+function rotateOut3(){
+  changeRotate3.style.width = "100px";
+  iconChangerotate3.style.transform = 'rotate(360deg)';
+}
+
+// Khi hover thì đổi box và rotate icon 4
+const changeRotate4 = document.getElementById("social-bar-4");
+const iconChangerotate4 = document.getElementById("icon-rotate-js4");
+
+changeRotate4.addEventListener("mouseover", rotateIn4);
+changeRotate4.addEventListener("mouseout", rotateOut4);
+
+function rotateIn4(){
+  changeRotate4.style.width = "400px";
+  iconChangerotate4.style.transform = 'rotate(0deg)';
+}
+
+function rotateOut4(){
+  changeRotate4.style.width = "100px";
+  iconChangerotate4.style.transform = 'rotate(360deg)';
+}
+
+
 // Mở chatbox
 const chatBoxbtn = document.querySelector('.js-chatbox')
 const closeChatbox = document.querySelector('.js-close-chatbox')
 const modal = document.querySelector('.js-modal')
 const modalContainer = document.querySelector('.modal-container')
-
-// chatBoxbtn.onclick = function(){
-//     modalContainer.style.transform = "translateX(0)";
-//     modal.style.display="flex";
-// }
-
-// closeChatbox.onclick = function(){
-//     modalContainer.style.transform = "translateX(100%)";
-//     modal.style.display="none";
-// }
 
 // Hàm hiển thị chatbox (thêm class open vào modal)
 function showChatbox(){
@@ -61,7 +121,15 @@ closeChatbox.addEventListener('click', hideChatbox)
 
 
 
+// chatBoxbtn.onclick = function(){
+//     modalContainer.style.transform = "translateX(0)";
+//     modal.style.display="flex";
+// }
 
+// closeChatbox.onclick = function(){
+//     modalContainer.style.transform = "translateX(100%)";
+//     modal.style.display="none";
+// }
 // Thực hiện animation khi mử
 // chatBoxbtn.onclick = function(){
 //   var isChangedOpen = modalContainer.ClientAnimation === modalContainerAnimation;
